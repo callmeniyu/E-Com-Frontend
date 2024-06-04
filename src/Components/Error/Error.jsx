da6cb8f776f7994e4453c9d1dde19faa9573f8a7
callmeniyu <getniyashere@gmail.com>
2024-06-04 07:14:09 +0530

    resolve page refresh and direct access of url problem on deployment

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import "./Error.css"
 
 const Error = () => {
   return (
-    <div><h1>Error</h1></div>
+      <div className='error'>
+          <h1>404</h1>
+          <h3>Page Not Found</h3>
+          <Link className='error-btn' to="/">Go Home</Link>
+      </div>
   )
 }
 
